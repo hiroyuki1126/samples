b4c4a026fabec47dc300f7403700adb6c19b84f8
Hiroyuki Ishihara <ishihara.hiroyuki3@jp.panasonic.com>
2017-12-26 17:12:36 +0900

    mode機能を追加。

--- ruby/gen_random_str.rb
+++ ruby/gen_random_str.rb
@@ -1,11 +1,25 @@
 #!/usr/bin/env ruby
 
-def gen_random_str(str_len = 8)
-  return (0...str_len).map{(65 + rand(26)).chr}.join
+def gen_random_str(mode = :mode3, str_len = 8)
+  case mode
+  when :mode1
+    o = [('a'..'z')].map { |i| i.to_a }.flatten
+  when :mode2
+    o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
+  when :mode3
+    o = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map { |i| i.to_a }.flatten
+  end
+  return (0...str_len).map { o[rand(o.length)] }.join
 end
 
-puts 'ランダム文字列を出力する（8文字）。'
+puts 'ランダム文字列（mode3, 8文字）'
 puts gen_random_str
 
-puts 'ランダム文字列を出力する（20文字）。'
-puts gen_random_str(20)
+puts 'ランダム文字列（mode1, 8文字）'
+puts gen_random_str(:mode1)
+
+puts 'ランダム文字列（mode2, 10文字）'
+puts gen_random_str(:mode2, 10)
+
+puts 'ランダム文字列（mode3, 20文字）'
+puts gen_random_str(:mode3, 20)
