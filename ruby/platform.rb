99594d63f0d1e12446505337bc29a79f40d3080b
Hiroyuki Ishihara <hiroyuki.1126@icloud.com>
2017-12-26 22:03:26 +0900

    軽微な修正。

--- ruby/platform.rb
+++ ruby/platform.rb
@@ -2,11 +2,11 @@
 
 case RUBY_PLATFORM
 when /x86_64-darwin17/
-  puts 'This machine is macOS'
+  puts 'This machine is macOS.'
 when /i386-mingw32/
   puts 'This machine is Windows.'
 when /x86_64-linux/
-  puts 'This machine is Linux'
+  puts 'This machine is Linux.'
 else
   puts 'This machine is unknown.'
 end
