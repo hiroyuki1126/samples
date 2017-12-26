9f213d36346257ffbc5619bbed02b66003307aa5
Hiroyuki Ishihara <ishihara.hiroyuki3@jp.panasonic.com>
2017-12-26 17:52:12 +0900

    print_messageメソッドを追加。

--- ruby/strftime.rb
+++ ruby/strftime.rb
@@ -2,8 +2,13 @@
 
 require 'date'
 
-puts '"年月日_時分秒"を出力する。'
+def print_message(msg)
+  now = DateTime.now.strftime('%Y/%m/%d %H:%M:%S')
+  STDERR.puts "[#{now}] #{msg}"
+end
+
+puts '"年月日_時分秒"を取得する。'
 puts Time.now.strftime('%Y%m%d_%H%M%S')
 
-puts '"年/月/日 時:分:秒"を出力する。'
-puts Time.now.strftime('%Y/%m/%d %H:%M:%S')
+puts '日時を含めてメッセージを出力する。'
+print_message 'hello, world'
