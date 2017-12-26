f37318de5619213e00d05e738c831962f4b3338b
Hiroyuki Ishihara <ishihara.hiroyuki3@jp.panasonic.com>
2017-12-26 16:55:13 +0900

    ファイル名を変更。

--- ruby/gen_random_str.rb
+++ ruby/gen_random_str.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
 
-def generate_random_str(str_len = 8)
+def gen_random_str(str_len = 8)
   return (0...str_len).map{(65 + rand(26)).chr}.join
 end
 
 puts 'ランダム文字列を出力する（8文字）。'
-puts generate_random_str
+puts gen_random_str
 
 puts 'ランダム文字列を出力する（20文字）。'
-puts generate_random_str(20)
+puts gen_random_str(20)
